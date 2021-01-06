7ac34af0fffd1849f7ec92ae395d0c66f5cdc85d
twostars <i.am.twostars@gmail.com>
2021-01-07 03:00:39 +1100

    Don't require signing for release build This should only be used by release-signed

--- build-release.bat
+++ build-release.bat
@@ -4,7 +4,7 @@ if exist logs goto msbuild
 mkdir logs
 
 :msbuild
-"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\MSBUILD.exe" HostsFileEditor.proj /t:Build /p:Configuration=Release /p:StrongName=true /l:FileLogger,Microsoft.Build.Engine;logfile=logs\build-release.log
+"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\MSBUILD.exe" HostsFileEditor.proj /t:Build /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=logs\build-release.log
 goto end
 
 :end
